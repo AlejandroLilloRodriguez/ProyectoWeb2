648d34f065b2c1c12135ee4a6feab3552db4062b
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2026-05-04 13:26:16 +0200

    conexion a api externa

--- services/api-externa.js
+++ services/api-externa.js
@@ -19,6 +19,27 @@ function fetchJSON(url,headers = {}) {
         });
     });}
 
+
+
+function fetchXML(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = '';
+
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(data));
+    }).on('error', reject);
+  });
+}
+
+async function getexternalData() {
+    return await fetchJSON('https://api.balldontlie.io/v1/teams');
+}
+async function getexternalXML() {
+    return await fetchXML('https://sports.yahoo.com/nba/rss.xml');
+}
+
 module.exports = {
-    fetchJSON
+    getexternalData,
+    getexternalXML
 };
